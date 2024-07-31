a006fb6790c13839811b5559a97a4c2e66927108
Brady Barker <brady.d.barker@gmail.com>
2024-07-30 22:40:29 -0400

    Created hook to fetch data from pokemon api. Currently restricted due to rate limits :(. Able to fetch data, parse it, put it in local storage, and grab it from local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 // import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { pokemonNames } from './data/pokemonNames'
 
 function App() {
+  const [pokemonObjects, setPokemonObjects] = useState(() => JSON.parse(localStorage.getItem('pokemonJson') ?? '[]'));
+
+  useEffect(() => {
+    async function getPokemonObjects() {
+      const fetchRequests = pokemonNames.map((pokemon) => {
+        try {
+          fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        } catch (e) {
+          console.log(`Error: ${e}`);
+        }
+      });
+      const fetchResponses = await Promise.all(fetchRequests);
+
+      if (!fetchResponses.includes(undefined)) {
+        const pokemon = await Promise.all(fetchResponses.map((response) => response.json()));
+        setPokemonObjects(pokemon.map((pokemonObj) => (
+          { 'id': pokemonObj.id, 'name': pokemonObj.name, 'sprites': pokemonObj.sprites.other.dream_world.front_default }))
+        )
+        localStorage.setItem('pokemonJson', JSON.stringify(pokemonObjects));
+      } else {
+        setPokemonObjects({ id: 1, name: 'test', sprites: 'http://test.com' })
+      }
+    }
+
+
+    console.log(pokemonObjects)
+    if (pokemonObjects.length === 0) {
+      console.log("Fetching DATA!")
+      getPokemonObjects();
+    } else {
+      console.log("In local storage")
+    }
+  }, [pokemonObjects])
+
   return (
     <>
     </>
---
+++ src/data/pokemonNames.js
@@ -0,0 +1,20 @@
+/* const pokemonNames = [
+    'psyduck',
+    'mewtwo',
+    'pikachu',
+    'suicune',
+    'charizard',
+    'rayquaza',
+    'gengar',
+    'blaziken',
+    'greninja',
+    'garchomp',
+    'decidueye',
+    'bulbasaur'
+]; */
+
+const pokemonNames = [
+    'psyduck',
+];
+
+export { pokemonNames }
