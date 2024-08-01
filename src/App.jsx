6b8d906796d8ba0e75038f3b48a631528a35bfc2
Brady Barker <brady.d.barker@gmail.com>
2024-07-31 21:09:43 -0400

    Fetch data from pokeApi. Place data in state variable and localStorage. On mount will look through localStorage first

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
 // import { useState } from 'react'
 import { useEffect, useState } from 'react'
 import './App.css'
-import { pokemonNames } from './data/pokemonNames'
+
+// { 'id': pokemon.id, 'name': pokemonObj.name, 'sprites': pokemonObj.sprites.other.dream_world.front_default }
 
 function App() {
-  const [pokemonObjects, setPokemonObjects] = useState(() => JSON.parse(localStorage.getItem('pokemonJson') ?? '[]'));
+  const [pokemonObjects, setPokemonObjects] = useState(() => JSON.parse(localStorage.getItem('pokemonJson') ?? []));
 
   useEffect(() => {
     async function getPokemonObjects() {
-      const fetchRequests = pokemonNames.map((pokemon) => {
-        try {
-          fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        } catch (e) {
-          console.log(`Error: ${e}`);
-        }
-      });
-      const fetchResponses = await Promise.all(fetchRequests);
+      try {
+        const fetchRequest = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=12&limit=12');
+        const fetchResponse = await fetchRequest.json();
+        const pokemonArray = fetchResponse.results;
 
-      if (!fetchResponses.includes(undefined)) {
-        const pokemon = await Promise.all(fetchResponses.map((response) => response.json()));
-        setPokemonObjects(pokemon.map((pokemonObj) => (
-          { 'id': pokemonObj.id, 'name': pokemonObj.name, 'sprites': pokemonObj.sprites.other.dream_world.front_default }))
-        )
-        localStorage.setItem('pokemonJson', JSON.stringify(pokemonObjects));
-      } else {
+        const pokemonRequests = await Promise.all(pokemonArray.map((pokemon) => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)));
+        const pokemon = await Promise.all(pokemonRequests.map((pokemon) => pokemon.json()));
+
+        setPokemonObjects(pokemon)
+        localStorage.setItem('pokemonJson', JSON.stringify(pokemon));
+      } catch (error) {
+        console.log(error)
         setPokemonObjects({ id: 1, name: 'test', sprites: 'http://test.com' })
       }
     }
 
-
-    console.log(pokemonObjects)
-    if (pokemonObjects.length === 0) {
-      console.log("Fetching DATA!")
-      getPokemonObjects();
-    } else {
-      console.log("In local storage")
-    }
+    pokemonObjects.length === 0 && getPokemonObjects();
   }, [pokemonObjects])
+
 
   return (
     <>
